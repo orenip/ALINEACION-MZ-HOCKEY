a7f9bb958868f0d5cefee4b100ecd798b872b96d
Jose Antonio López Piñero <josanlopi@gmail.com>
2023-11-21 21:24:18 +0100

    ultimas pruebas ejecutable

--- app.py
+++ app.py
@@ -1,76 +1,86 @@
 from flask import Flask, render_template
 import pandas as pd
+import os
+import sys
 
 app = Flask(__name__)
 
 # Ruta principal del programa
 @app.route('/')
 def index():
-    # Lee el archivo Excel
-    df = pd.read_excel('tu_archivo_excel.xlsx')
+    # Obtén la ruta del directorio donde se encuentra este script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
 
-    # Elimina filas con valores nulos en columnas relevantes
-    df = df.dropna(subset=['DEF', 'TOTALES'])
+    # Construye la ruta al archivo Excel, manejando el caso del entorno temporal (_MEIxxxxxx)
+    if getattr(sys, 'frozen', False):
+        # Se está ejecutando como ejecutable empaquetado
+        excel_file_path = os.path.join(os.path.dirname(sys.executable), 'tu_archivo_excel.xlsx')
+    else:
+        # No se está ejecutando como ejecutable empaquetado
+        excel_file_path = os.path.join(script_dir, 'tu_archivo_excel.xlsx')
 
-    # Verifica los nombres exactos de las columnas en tu archivo Excel
-    required_columns = ['Numero', 'NOMBRE', 'CEN', 'ALA', 'DEF', 'CTL', 'TOTALES']
-    missing_columns = [col for col in required_columns if col not in df.columns]
+    # Verifica si el archivo existe antes de intentar abrirlo
+    if not os.path.exists(excel_file_path):
+        return f"Error: El archivo Excel no se encuentra en la ruta especificada: {excel_file_path}"
 
-    if missing_columns:
-        return f"Error: Las siguientes columnas no se encontraron en el archivo Excel: {', '.join(missing_columns)}"
 
-    # Encuentra los 4 mejores en la columna CEN con CTL >= 8
-    top_cen = df[df['CTL'] >= 8].nlargest(4, 'CEN')[['Numero', 'NOMBRE', 'CEN']]
+    # Resto del código para leer y procesar el archivo Excel
+    try:
+        # Lee el archivo Excel
+        df = pd.read_excel(excel_file_path)
 
-    # Crea una nueva columna 'Posicion' basada en el valor mayor entre ALA y DEF
-    df['Posicion'] = df.apply(lambda row: 'ALA' if row['ALA'] > row['DEF'] else 'DEF', axis=1)
+        # Elimina filas con valores nulos en columnas relevantes
+        df = df.dropna(subset=['DEF', 'TOTALES'])
 
-    # Encuentra los 8 mejores en la columna ALA que no estén en CEN
-    top_ala = df[(df['Posicion'] == 'ALA') & ~df['Numero'].isin(top_cen['Numero'])].nlargest(8, 'ALA')[['Numero', 'NOMBRE', 'ALA', 'TOTALES']]
+        # Verifica los nombres exactos de las columnas en tu archivo Excel
+        required_columns = ['Numero', 'NOMBRE', 'CEN', 'ALA', 'DEF', 'CTL', 'TOTALES']
+        missing_columns = [col for col in required_columns if col not in df.columns]
 
-    # Encuentra los 8 mejores en la columna DEF que no estén en CEN
-    top_def = df[(df['Posicion'] == 'DEF') & ~df['Numero'].isin(top_cen['Numero'])].nlargest(8, 'DEF')[['Numero', 'NOMBRE', 'DEF', 'TOTALES']]
+        if missing_columns:
+            return f"Error: Las siguientes columnas no se encontraron en el archivo Excel: {', '.join(missing_columns)}"
 
-    # Si hay empate en DEF, selecciona el jugador con mayor valor en TOTALES
-    if len(top_def) < 8:
-        # Elimina las filas con NaN en 'DEF' y 'TOTALES'
-        top_def = top_def.dropna(subset=['DEF', 'TOTALES'])
+        # Encuentra los 4 mejores en la columna CEN con CTL >= 8
+        top_cen = df[df['CTL'] >= 8].nlargest(4, 'CEN')[['Numero', 'NOMBRE', 'CEN']]
 
-        # Rellena con NaN si aún no hay 8 jugadores en 'DEF'
-        nan_rows = pd.DataFrame([[float('nan')]*4 for _ in range(8 - len(top_def))], columns=['Numero', 'NOMBRE', 'DEF', 'TOTALES'])
-        top_def = pd.concat([top_def, nan_rows], ignore_index=True)
+        # Crea una nueva columna 'Posicion' basada en el valor mayor entre ALA y DEF
+        df['Posicion'] = df.apply(lambda row: 'ALA' if row['ALA'] > row['DEF'] else 'DEF', axis=1)
 
-    # Si hay empate en ALA, selecciona el jugador con mayor valor en TOTALES
-    if len(top_ala) < 8 and top_ala['ALA'].diff().min() < 0.75:
-        top_ala = top_ala.dropna(subset=['ALA', 'TOTALES'])
+        # Encuentra los 8 mejores en la columna ALA que no estén en CEN
+        top_ala = df[(df['Posicion'] == 'ALA') & ~df['Numero'].isin(top_cen['Numero'])].nlargest(8, 'ALA')[['Numero', 'NOMBRE', 'ALA', 'TOTALES']]
 
-        # Rellena con NaN si aún no hay 8 jugadores en 'DEF'
-        nan_rows = pd.DataFrame([[float('nan')]*4 for _ in range(8 - len(top_ala))], columns=['Numero', 'NOMBRE', 'ALA', 'TOTALES'])
-        top_ala = pd.concat([top_ala, nan_rows], ignore_index=True)
+        # Encuentra los 8 mejores en la columna DEF que no estén en CEN
+        top_def = df[(df['Posicion'] == 'DEF') & ~df['Numero'].isin(top_cen['Numero'])].nlargest(8, 'DEF')[['Numero', 'NOMBRE', 'DEF', 'TOTALES']]
 
-    # Encuentra las reservas para ALA (los 2 mejores que no están en ALA, CEN ni DEF)
-    reservas_ala = df[(df['Posicion'] == 'ALA') & ~df['Numero'].isin(top_cen['Numero']) & ~df['Numero'].isin(top_ala['Numero']) & ~df['Numero'].isin(top_def['Numero'])].nlargest(2, 'ALA')[['Numero', 'NOMBRE', 'ALA', 'TOTALES']]
+        # Resto del código para procesar las reservas y manejar empates
 
-    # Encuentra las reservas para DEF (los 2 mejores que no están en ALA, CEN ni DEF)
-    reservas_def = df[(df['Posicion'] == 'DEF') & ~df['Numero'].isin(top_cen['Numero']) & ~df['Numero'].isin(top_ala['Numero']) & ~df['Numero'].isin(top_def['Numero'])].nlargest(2, 'DEF')[['Numero', 'NOMBRE', 'DEF', 'TOTALES']]
+        # Encuentra las reservas para ALA (los 2 mejores que no están en ALA, CEN ni DEF)
+        reservas_ala = df[(df['Posicion'] == 'ALA') & ~df['Numero'].isin(top_cen['Numero']) & ~df['Numero'].isin(top_ala['Numero']) & ~df['Numero'].isin(top_def['Numero'])].nlargest(2, 'ALA')[['Numero', 'NOMBRE', 'ALA', 'TOTALES']]
 
-    # Verifica si un reserva tiene más totales que un top y la diferencia en DEF o ALA es menor a 0.75, luego reemplaza el top por el reserva
-    for reserva, top, columna in zip([reservas_ala, reservas_def], [top_ala, top_def], ['ALA', 'DEF']):
-        for index, row in reserva.iterrows():
-            for top_index, top_row in top.iterrows():
-                if row['TOTALES'] > top_row['TOTALES'] and abs(row[columna] - top_row[columna]) < 0.75:
-                    # Encuentra el siguiente jugador con más totales
-                    siguiente_mejor = df[(df['Posicion'] == columna) & ~df['Numero'].isin(top_cen['Numero']) & ~df['Numero'].isin(top_ala['Numero']) & ~df['Numero'].isin(top_def['Numero']) & (df['Numero'] != row['Numero'])].nlargest(1, 'TOTALES')
-                    
-                    # Actualiza el top con el siguiente mejor jugador
-                    top.at[top_index, 'Numero'] = siguiente_mejor['Numero'].values[0]
-                    top.at[top_index, 'NOMBRE'] = siguiente_mejor['NOMBRE'].values[0]
-                    top.at[top_index, columna] = siguiente_mejor[columna].values[0]
-                    top.at[top_index, 'TOTALES'] = siguiente_mejor['TOTALES'].values[0]
+        # Encuentra las reservas para DEF (los 2 mejores que no están en ALA, CEN ni DEF)
+        reservas_def = df[(df['Posicion'] == 'DEF') & ~df['Numero'].isin(top_cen['Numero']) & ~df['Numero'].isin(top_ala['Numero']) & ~df['Numero'].isin(top_def['Numero'])].nlargest(2, 'DEF')[['Numero', 'NOMBRE', 'DEF', 'TOTALES']]
 
-    return render_template('index.html', top_cen=top_cen.values.tolist(), top_ala=top_ala.values.tolist(),
-                           top_def=top_def.values.tolist(), reservas_ala=reservas_ala.values.tolist(),
-                           reservas_def=reservas_def.values.tolist())
+        # Verifica si un reserva tiene más totales que un top y la diferencia en DEF o ALA es menor a 0.75, luego reemplaza el top por el reserva
+        for reserva, top, columna in zip([reservas_ala, reservas_def], [top_ala, top_def], ['ALA', 'DEF']):
+            for index, row in reserva.iterrows():
+                for top_index, top_row in top.iterrows():
+                    if row['TOTALES'] > top_row['TOTALES'] and abs(row[columna] - top_row[columna]) < 0.75:
+                        # Encuentra el siguiente jugador con más totales
+                        siguiente_mejor = df[(df['Posicion'] == columna) & ~df['Numero'].isin(top_cen['Numero']) & ~df['Numero'].isin(top_ala['Numero']) & ~df['Numero'].isin(top_def['Numero']) & (df['Numero'] != row['Numero'])].nlargest(1, 'TOTALES')
+                        
+                        # Actualiza el top con el siguiente mejor jugador
+                        top.at[top_index, 'Numero'] = siguiente_mejor['Numero'].values[0]
+                        top.at[top_index, 'NOMBRE'] = siguiente_mejor['NOMBRE'].values[0]
+                        top.at[top_index, columna] = siguiente_mejor[columna].values[0]
+                        top.at[top_index, 'TOTALES'] = siguiente_mejor['TOTALES'].values[0]
+
+
+        return render_template('index.html', top_cen=top_cen.values.tolist(), top_ala=top_ala.values.tolist(),
+                               top_def=top_def.values.tolist(), reservas_ala=reservas_ala.values.tolist(),
+                               reservas_def=reservas_def.values.tolist())
+    except Exception as e:
+        return f"Error al procesar el archivo Excel: {str(e)}"
+
 
 if __name__ == '__main__':
+    # Ejecuta la aplicación Flask con el modo de depuración activado
     app.run(debug=True)
